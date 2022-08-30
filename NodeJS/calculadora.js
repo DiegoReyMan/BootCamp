672863714357144e2a626596c46a4500029f3e02
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-08-30 09:57:49 -0300

    cambios en calculadora

---
+++ NodeJS/calculadora.js
@@ -0,0 +1,14 @@
+// Importa funciones de la calculadora
+
+const suma = require('./sumar');
+const resta = require('./restar');
+const multipcacion = require('./multiplicar');
+const divicion = require('./dividir');
+
+
+console.log(suma(1, 2));
+console.log(resta(1, 2));
+console.log(multipcacion(1, 2));
+console.log(divicion(1, 3));
+console.log(divicion(3, 0)); 
+
