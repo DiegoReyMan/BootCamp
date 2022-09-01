dbec23f2e42e41759223beeffd82d9acd0126132
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-09-01 11:14:53 -0300

    Tareas2

--- NodeJS2/app.js
+++ NodeJS2/app.js
@@ -1,5 +1,17 @@
 const leer = require('./funcionesDeTareas');
 
-console.log(leer("./tareas.json"));
+let data = leer('./tareas.json'); 
 
+let command = process.argv[2];
+
+switch (command) {
+    case 'listar' :
+        console.table(data);
+        break;
+    case undefined :
+        console.log('Atención-Tienes que pasar una acción.');
+        break;
+    default :
+        console.log('No entiendo que quieres hacer');
+}
 
