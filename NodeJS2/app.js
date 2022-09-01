1c5f4d749dac524324de958f558864aea07b300f
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-09-01 11:52:07 -0300

    Tareas2 con midificaciones

--- NodeJS2/app.js
+++ NodeJS2/app.js
@@ -1,17 +1,6 @@
 const leer = require('./funcionesDeTareas');
 
-let data = leer('./tareas.json'); 
-
 let command = process.argv[2];
 
-switch (command) {
-    case 'listar' :
-        console.table(data);
-        break;
-    case undefined :
-        console.log('Atención-Tienes que pasar una acción.');
-        break;
-    default :
-        console.log('No entiendo que quieres hacer');
-}
+let data = leer('./tareas.json', command);
 
--- NodeJS2/funcionesDeTareas.js
+++ NodeJS2/funcionesDeTareas.js
@@ -1,9 +1,20 @@
 const fd = require("fs");
 
-const leerJson = (path) => {
+const leerJson = (path, command) => {
     let dataJson = fd.readFileSync("./tareas.json", "utf8");
     let data = JSON.parse(dataJson);
-    return data;
+    //return data;
+    switch (command) {
+        case 'listar' :
+            console.table(data);
+            break;
+        case undefined :
+            console.log('Atención-Tienes que pasar una acción.');
+            break;
+        default :
+            console.log('No entiendo que quieres hacer');
+    }
+    
 }
 
 module.exports = leerJson;
