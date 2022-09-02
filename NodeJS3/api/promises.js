ca9d0b46cf0d5a9cb13595e1514d14e62fecb975
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-09-02 13:58:07 -0300

    NodeJS3 Tarea terminada

--- NodeJS3/api/promises.js
+++ NodeJS3/api/promises.js
@@ -1,6 +1,7 @@
 const axios = require('axios').default;
 const url = require('../api/apiData');
 
+// Funcion que devuelve una promesa con sus correspondientes error y data
 const data = async() => console.log(await axios.get(url));
 
 module.exports = data;
