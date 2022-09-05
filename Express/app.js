48b7f1fbfadb278d214030a68f0b48910a0b9f12
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-09-05 15:52:15 -0300

    Express

--- Express/app.js
+++ Express/app.js
@@ -3,15 +3,19 @@ const app = express();
 
 app.listen(3000, () => console.log('El servidor esta corriendo'));
 
-app.get('/', function(req, res){
+app.get('/',(req, res) => {
     res.send('Home')
 });
 
-app.get('/contacto', function(req, res){
+app.get('/contacto', (req, res) => {
     res.send('Contacto')
 });
 
-app.get('/productos/:idProducto', function(req, res){
+app.get('/productos/:idProducto', (req, res) => {
     const porducto = req.params.idProducto;
     res.send(`Esta es la pagina del producto ${porducto}`)
+});
+
+app.get('*', (req, res) => {
+    res.send(`Error 404`)
 });
