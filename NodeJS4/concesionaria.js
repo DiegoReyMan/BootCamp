6005a5bef37d397d741a6e5b30e877dfdaef69e1
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-09-05 10:50:26 -0300

    NodeJS4 avances

--- NodeJS4/concesionaria.js
+++ NodeJS4/concesionaria.js
@@ -5,23 +5,32 @@ let concesionaria = {
 
     autos: autos,
 
-    buscarAuto: function(patente) {
-        let res = null;
-        this.autos.forEach(auto => {
-            if (auto.patente === patente) {
-                res = auto;
-            }
-        });
-        return res;
+    // buscarAuto: function(patente) {
+    //     let res = null;
+    //     this.autos.forEach(auto => {
+    //         if (auto.patente === patente) {
+    //             res = auto;
+    //         }
+    //     });
+    //     return res;
+    // },
+
+    buscarAuto: function(patente){
+        const res = this.autos.find( auto => auto.patente === patente)
+        if (res == undefined) {
+            return null  
+        } else {
+            return res
+        }
     },
 
     venderAuto: function(patente) {
         const auto = this.buscarAuto(patente);
-        if (auto != null) {
+        if (!auto) {
             auto.vendido = true;
         }
     }
 }
 
-console.log(concesionaria.venderAuto('JJK116'))
+//console.log(concesionaria.venderAuto('JJK116'))
 console.log(concesionaria.buscarAuto('JJK116'));
