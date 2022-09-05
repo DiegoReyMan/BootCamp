9878e96d98e0179875b623793ebfb7ea9f449c82
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-09-05 16:14:10 -0300

    Express

--- Express/app.js
+++ Express/app.js
@@ -7,13 +7,17 @@ app.get('/',(req, res) => {
     res.send('Home')
 });
 
+app.get('/producto', (req, res) => {
+    res.send('Producto')
+});
+
 app.get('/contacto', (req, res) => {
     res.send('Contacto')
 });
 
-app.get('/productos/:idProducto', (req, res) => {
-    const porducto = req.params.idProducto;
-    res.send(`Esta es la pagina del producto ${porducto}`)
+app.get('/contacto/:idContacto', (req, res) => {
+    const contacto = req.params.idContacto;
+    res.send(`Esta es la pagina del contacto ${contacto}`)
 });
 
 app.get('*', (req, res) => {
