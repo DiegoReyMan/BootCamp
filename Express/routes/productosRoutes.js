516583698bac406c1d250503b2020bc50dea47ed
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-09-06 11:39:15 -0300

    Express Avances

--- Express/routes/productosRoutes.js
+++ Express/routes/productosRoutes.js
@@ -1,7 +1,5 @@
 const express = require('express');
- 
 const router = express.Router();
-
 const productosCtr = require('../controllers/productosController');
 
 router.get('/', productosCtr.listarProductos)
