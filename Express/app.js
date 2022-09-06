c117e208cd0ec429ef517f272b4632ad75b34ce9
DiegoReyMan <diegoleandrorey@G500606MAC329.local>
2022-09-06 12:54:38 -0300

    Express Avances

--- Express/app.js
+++ Express/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const app = express();
 
 // Requiere las rutas
 const rutasProductos = require('./routes/productosRoutes');
 const rutasContacto = require('./routes/usuariosRoutes');
 
-
-const app = express();
-
-//Levanta el servidor 
+// Levanta el servidor 
 app.listen(3000, () => console.log('El servidor esta corriendo'));
 
 // Atiende a /productos y /usuarios
